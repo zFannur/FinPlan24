9f3875ecec0ad4791fc1b36346625237afd174ea
zfannur <zfannur@mail.ru>
2024-01-04 11:50:33 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.finplan24
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:finplan24/app/app.dart';
+import 'package:finplan24/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const FinPlan24App());
+}
